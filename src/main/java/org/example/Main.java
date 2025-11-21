b2cfd31edea9c91e8b5e644b8d6aadd6509f7cc7
Rabe3 <a.rabe3.425@gmail.com>
2025-11-21 14:48:48 +0200

    changed LookAndFeel of project

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,12 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
+        try{
+            UIManager.setLookAndFeel(new com.formdev.flatlaf.FlatIntelliJLaf());
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
         JFrame frame = new MainPanel();
     }
 }
