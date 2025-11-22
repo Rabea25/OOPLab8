7e9eac7eb842a1531192a8daab889530729c0849
eyad-amir <amireyad93@gmail.com>
2025-11-22 22:12:11 +0200

    Admin dash board done

--- src/main/java/org/example/Course.java
+++ src/main/java/org/example/Course.java
@@ -1,9 +1,7 @@
 package org.example;
 
 import java.util.ArrayList;
-import java.util.List;
-
-;
+import java.util.List;;
 public class Course {
     private String description;
     private String title;
